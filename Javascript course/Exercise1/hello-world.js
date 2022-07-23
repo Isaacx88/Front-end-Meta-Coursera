821eb071cf9bcdd8224d2abacdfb4b2ed7d7aa50
Isaac <isaacbarros88@gmail.com>
2022-07-23 15:32:00 -0300

    Arrays and functions

--- Javascript course/Exercise1/hello-world.js
+++ Javascript course/Exercise1/hello-world.js
@@ -26,47 +26,61 @@
 //     console.log("%c" + cubes[i], styles)
 // }
 
-for (var i = 1; i<=10; i++){
-    if(i==1){
-        console.log("Gold medal");
-    } else if (i==2){
-        console.log("Silver medal");
-    } else if (i==3){
-        console.log("Bronze medal");
-    } else {
-        console.log(i);
+// for (var i = 1; i<=10; i++){
+//     if(i==1){
+//         console.log("Gold medal");
+//     } else if (i==2){
+//         console.log("Silver medal");
+//     } else if (i==3){
+//         console.log("Bronze medal");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// for (var i = 1; i<=10; i++){
+// switch(i){
+//     case 1:
+//         console.log("Gold medal");
+//         break;
+//     case 2:
+//         console.log("Silver medal");
+//         break;
+//     case 3:
+//         console.log("Bronze medal");
+//         break;
+//     default:
+//         console.log(i)
+// }
+// }
+
+
+// //Functions
+// function myFunc(){
+//     console.log("This is the structure of a function")
+// }
+
+// //Calling functions
+// myFunc();
+
+// //Using parameters
+// function sumNumbers(a, b){
+//     var sum = a + b;
+//     return console.log("The sum of", a, "and", b, "is", sum);
+// }
+
+// sumNumbers(10,20);
+
+//practice
+
+function letterFinder (word, match){
+    for(var i = 0; i < word.length ; i++){
+        if(word[i] == match){
+            console.log('Found the', match, 'at', i);
+        } else {
+            console.log('---No match found at', i);
+        }
     }
 }
 
-for (var i = 1; i<=10; i++){
-switch(i){
-    case 1:
-        console.log("Gold medal");
-        break;
-    case 2:
-        console.log("Silver medal");
-        break;
-    case 3:
-        console.log("Bronze medal");
-        break;
-    default:
-        console.log(i)
-}
-}
-
-
-//Functions
-function myFunc(){
-    console.log("This is the structure of a function")
-}
-
-//Calling functions
-myFunc();
-
-//Using parameters
-function sumNumbers(a, b){
-    var sum = a + b;
-    return console.log("The sum of", a, "and", b, "is", sum);
-}
-
-sumNumbers(10,20);
+letterFinder("test", "t");
