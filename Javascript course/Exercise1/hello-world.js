f8d71e7d27f45c71ca151bb79d9fcf088bc442e9
Isaac <isaacbarros88@gmail.com>
2022-07-23 15:06:35 -0300

    Functions in JS

--- Javascript course/Exercise1/hello-world.js
+++ Javascript course/Exercise1/hello-world.js
@@ -19,9 +19,54 @@
 // }
 // console.log('Countdown finished!');
 
-var cubes = 'ABCDEFG';
+// var cubes = 'ABCDEFG';
 
-for (var i = 0; i < cubes.length; i++) {
-    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
-    console.log("%c" + cubes[i], styles)
+// for (var i = 0; i < cubes.length; i++) {
+//     var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+//     console.log("%c" + cubes[i], styles)
+// }
+
+for (var i = 1; i<=10; i++){
+    if(i==1){
+        console.log("Gold medal");
+    } else if (i==2){
+        console.log("Silver medal");
+    } else if (i==3){
+        console.log("Bronze medal");
+    } else {
+        console.log(i);
+    }
 }
+
+for (var i = 1; i<=10; i++){
+switch(i){
+    case 1:
+        console.log("Gold medal");
+        break;
+    case 2:
+        console.log("Silver medal");
+        break;
+    case 3:
+        console.log("Bronze medal");
+        break;
+    default:
+        console.log(i)
+}
+}
+
+
+//Functions
+function myFunc(){
+    console.log("This is the structure of a function")
+}
+
+//Calling functions
+myFunc();
+
+//Using parameters
+function sumNumbers(a, b){
+    var sum = a + b;
+    return console.log("The sum of", a, "and", b, "is", sum);
+}
+
+sumNumbers(10,20);
