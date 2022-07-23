ddfbe280bb9b76257586d5aa5cf244e86406e18f
Isaac <isaacbarros88@gmail.com>
2022-07-23 16:53:56 -0300

    Strings cheat sheets and math-random

---
+++ Javascript course/Exercise1/math-random.js
@@ -0,0 +1,9 @@
+//generates an random number between 0 and 0.99
+Math.random();
+
+var decimal = Math.random();
+console.log(decimal);
+//If I want a number greater than 1
+console.log(decimal*10);
+
+
